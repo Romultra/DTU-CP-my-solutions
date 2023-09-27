cc4028a1ff3a817759326aa607409994af4b1010
Morten Hannemose <morten@hannemose.dk>
2023-09-27 18:25:42 +0200

    Week5

---
+++ cp/ex05/best_buy.py
@@ -0,0 +1,12 @@
+"""Exercise 5.11. Best Buy."""
+
+def best_buy(prices: list, money: int, start_index: int, reverse: bool) -> int:
+    """Return the number of items that can be bought with the given amount of money. The function should be able to handle arbitrary starting points and to run the list in reverse.
+    
+    :param prices: list of prices
+    :param money: amount of money
+    :param start_index: starting index in list
+    :param reverse: boolean to indicate if list should be run in reverse
+    :return: number of items that can be bought with the given amount of money
+    """
+    # TODO: Code has been removed from here. 
---
+++ cp/ex05/water_height.py
@@ -0,0 +1,10 @@
+"""Exercise 5.10. Water height."""
+
+def water_height(h0: int, r: list) -> int:
+    """Calculate the water height after multiple days of rain.
+
+    :param: h0: initial height of the water
+    :param: r: list of rain showers
+    :return: height of water after days of rain 
+    """
+    # TODO: Code has been removed from here. 
