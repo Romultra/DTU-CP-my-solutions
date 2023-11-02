d0d1eba16092353a9003c309fb849b8f911bd789
Romultra <romultra@gmail.com>
2023-11-02 14:05:05 +0100

    Added new exercices

---
+++ cp/ex07/astronomical_season.py
@@ -0,0 +1,10 @@
+"""Exercise 7.9: Astronomical season."""
+
+def astronomical_season(date :  tuple) -> str:
+    """Return the astronomical season of the given date.
+
+    :param date: Tuple with the given date.
+    :return: String with astronomical season
+    """
+    # TODO: Code has been removed from here. 
+    
---
+++ cp/ex07/morse_to_text.py
@@ -0,0 +1,9 @@
+"""Exercise 7.8: Morse code."""
+
+def morse_to_text(morse_code : str) -> str:
+    """Return the extracted message from its Morse code.
+
+    :param morse_code: String with the initial message encoded in Morse. 
+    :return: The decoded message.
+    """
+    # TODO: Code has been removed from here. 
