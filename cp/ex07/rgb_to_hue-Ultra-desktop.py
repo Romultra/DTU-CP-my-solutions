44ef11c8945164bfdc671f4aaf325f0bdc5c51ef
Romultra <romultra@gmail.com>
2023-11-09 00:43:43 +0100

    deleted duplicates

--- cp/ex07/rgb_to_hue-Ultra-desktop.py
+++
@@ -1,9 +0,0 @@
-"""Exercise 7.4: Color Hue."""
-
-def rgb_to_hue(RGB: tuple) -> float:
-    """Return the hue given RGB values.
-
-    :param RGB: Tuple containing  three RGB values.
-    :return: Hue in degrees.
-    """
-    # TODO: Code has been removed from here. 
